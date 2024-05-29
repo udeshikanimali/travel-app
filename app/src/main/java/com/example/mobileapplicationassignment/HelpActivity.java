9318aa31cb4508031d73ead333ed22f64902c6dc
udeshikanimali <udeshikanimali644@gmail.com>
2024-05-29 20:15:20 +0530

    changes

---
+++ app/src/main/java/com/example/mobileapplicationassignment/HelpActivity.java
@@ -0,0 +1,21 @@
+package com.example.mobileapplicationassignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class HelpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+
+        Window window = this.getWindow();
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(getResources().getColor(R.color.orange));
+    }
+}
--- app/src/main/java/com/example/mobileapplicationassignment/HomeActivity.java
+++ app/src/main/java/com/example/mobileapplicationassignment/HomeActivity.java
@@ -21,12 +21,21 @@ public class HomeActivity extends AppCompatActivity {
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
         window.setStatusBarColor(getResources().getColor(R.color.blue));
 
-        ImageView imageView = findViewById(R.id.imageView5);
+        ImageView imageView = findViewById(R.id.imageView9);
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Intent to start DetailsActivity
                 Intent intent = new Intent(HomeActivity.this, DetailsActivity.class);
+                startActivity(intent);
+            }
+        });
+        ImageView imageView1 = findViewById(R.id.imageView18);
+        imageView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Intent to start DetailsActivity
+                Intent intent = new Intent(HomeActivity.this, HelpActivity.class);
                 startActivity(intent);
             }
         });
